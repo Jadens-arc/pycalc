0b725cdc9788a6ce643aa864c57f698f6daa2a92
Jaden Arceneaux <arceneauxjaden@gmail.com>
2020-05-18 18:45:34 -0700

    init

---
+++ eval.py
@@ -0,0 +1,22 @@
+class Evaluator:
+    def __init__(self):
+        pass
+    def isNum(self, suspectedInteger):
+        try:
+            float(suspectedInteger)
+            return True
+        except:
+            return False
+
+    def interpret(self, userString):
+        userString = userString.split()
+        equation = ''
+        for word in userString:
+            if isNum(word):
+                equation += word
+            if word in ['+', '-', '/', '*']:
+                equation += word
+            if word == '*':
+                equation += '*'
+
+        return equaion
---
+++ main.py
@@ -0,0 +1,5 @@
+from eval import Evaluator
+
+myEval = Evaluate()
+
+
